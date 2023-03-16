40cf89dc26c9423829288ffd883024f4f21d4e08
satyam jha <jhasatyam386@gamil.com>
2023-03-16 12:09:56 +0530

    apicalling16_3_2023

--- apiCallingTopic15Work/ViewController.swift
+++ apiCallingTopic15Work/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+struct GetApi : Decodable{
+      var userId : Int
+      var id : Int
+      var title : String
+      var body : String
+   }
+
+
 class ViewController: UIViewController {
 
+    var arr : [GetApi] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getdata()
+    
     }
-
+    func getdata(){
+        let url = URL(string:"https://jsonplaceholder.typicode.com/posts")
+        var ur = URLRequest(url: url!)
+        ur.httpMethod = "GET"
+        URLSession.shared.dataTask(with: ur) { data ,response,error in
+            do {
+                if error == nil {
+                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
+                    print(self.arr)
+                }
+            }
+            catch{
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
+        
+    }
 
 }
 
