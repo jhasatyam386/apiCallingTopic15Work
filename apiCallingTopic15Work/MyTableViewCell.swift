458960235a09f23fc47451a496fdbad0068d2577
satyam jha <jhasatyam386@gamil.com>
2023-03-23 13:54:20 +0530

    update

---
+++ apiCallingTopic15Work/MyTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyTableViewCell.swift
+//  apiCallingTopic15Work
+//
+//  Created by R&W on 20/03/23.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
--- apiCallingTopic15Work/ViewController.swift
+++ apiCallingTopic15Work/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct GetApi : Decodable{
       var userId : Int
@@ -15,33 +16,82 @@ struct GetApi : Decodable{
    }
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+   
+    
 
     var arr : [GetApi] = []
     
+    @IBOutlet weak var tabelDataview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getdata()
     
     }
+//    func getdata(){
+//        let url = URL(string:"https://jsonplaceholder.typicode.com/posts")
+//        var ur = URLRequest(url: url!)
+//        ur.httpMethod = "GET"
+//        URLSession.shared.dataTask(with: ur) { data ,response,error in
+//            do {
+//                if error == nil {
+//                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
+//                    print(self.arr)
+//                    DispatchQueue.main.async {
+//                        self.tabelDataview.reloadData()
+//                    }
+//                }
+//            }
+//            catch{
+//                print(error.localizedDescription)
+//            }
+//
+//        }.resume()
+//
+//    }
+    
     func getdata(){
-        let url = URL(string:"https://jsonplaceholder.typicode.com/posts")
-        var ur = URLRequest(url: url!)
-        ur.httpMethod = "GET"
-        URLSession.shared.dataTask(with: ur) { data ,response,error in
-            do {
-                if error == nil {
-                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
-                    print(self.arr)
+       
+        AF.request("https://jsonplaceholder.typicode.com/posts",method: .get).responseData { respo in
+            switch respo.result{
+            case.success(let data):
+                do{
+                    if data == respo.value{
+                        self.arr = try JSONDecoder().decode([GetApi].self, from: data)
+                        print(self.arr)
+                        DispatchQueue.main.async{
+                            self.tabelDataview.reloadData()
+                        }
+                    }
                 }
+                catch{
+                    print(error.localizedDescription)
+                }
+            case.failure(let err):
+                print(err.localizedDescription)
             }
-            catch{
-                print(error.localizedDescription)
-            }
-            
-        }.resume()
+        }
+      
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tabelDataview.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
+        cell.label1.text = "\(arr[indexPath.row].userId)"
+        cell.label4.text = "\(arr[indexPath.row].id)"
+        cell.label2.text = arr[indexPath.row].title
+        cell.label3.text = arr[indexPath.row].body
+
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
     }
 
 }
